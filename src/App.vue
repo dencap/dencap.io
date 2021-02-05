fb37a4db5c4a85b5a0682838410ea5153ac0edca
Eric Kelley <hello@ericmkelley.com>
2021-02-05 14:08:39 -0500

    base

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css";
+
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <img src="https://cdn.emk.dev/dencap/favicon.png" width="50px" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+<style lang="scss" scoped>
+.hero {
+  background: rgb(247, 249, 252);
+  background: radial-gradient(
+    circle,
+    rgba(247, 249, 252, 1) 0%,
+    #c5cee0 100%
+  ) !important;
+  img {
+    width: 50px;
+  }
+}
+</style>
